00e5ed1093c0ed10afcaf69cfac6d57a8f446ecb
Ajsel Žilić <33603067+AveDominiInferni@users.noreply.github.com>
2023-02-19 23:41:40 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -20,7 +20,8 @@ app.listen(port, () => {
 const client = new Client({
     intents: [
         Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS
     ]
 });
 
